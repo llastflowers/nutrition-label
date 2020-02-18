6b7593fec11c8b08af8ed30b235b064749be8c74
Casey Martell <drmartell@gmail.com>
2020-02-17 18:05:53 -0800

    updates start script for Heroku back-end deployment

--- backend/lib/routes/details.routes.js
+++ backend/lib/routes/details.routes.js
@@ -5,6 +5,21 @@ module.exports = require('express').Router()
   .get('/:id', ({params: { id }}, res, next) => {
     fetch(`https://api.nal.usda.gov/fdc/v1/${id}?api_key=${process.env.API_KEY}`)
     .then(res => res.json())
-    .then(json => res.send(json))
+    .then(json => res.send(json.foodNutrients
+      .filter(nutrient => ['Protein',
+      'Sugars, total including NLEA',
+      'Energy',
+      'Cholesterol',
+      'Sodium',
+      'Total lipid (fat)',
+      'Fatty acids, total saturated',
+      'Carbohydrate, by difference',
+      'Fiber, total dietary']
+        .includes(nutrient.nutrient.name))
+        .map(nutrient => ({
+          name: nutrient.nutrient.name,
+          amount: nutrient.amount + ' ' + nutrient.nutrient.unitName
+        }))))
+        //.some(targetString => nutrient.nutrient.name.includes(targetString)))
     .catch(next);
   });
